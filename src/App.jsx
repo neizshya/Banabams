12868bd0f1fffa884b6679ae4359053fe98c5cda
neizshya <nurul.adzan24@gmail.com>
2023-05-09 07:23:36 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useCallback, useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Button from "react-bootstrap/Button";
 import { Alert } from "bootstrap";
